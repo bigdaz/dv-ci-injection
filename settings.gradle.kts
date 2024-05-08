eb4964c2ba451a4cee6100fa8c7a039c3ad9ac92
daz <daz@gradle.com>
2024-05-08 20:10:19 +0200

    Copy init-script and tests from 'gradle/actions'

---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("com.gradle.develocity") version("3.17.2")
+    id("com.gradle.common-custom-user-data-gradle-plugin") version("2.0.1")
+}
+
+develocity {
+    server = "https://ge.solutions-team.gradle.com"
+}
+
+rootProject.name = "develocity-injection-gradle-plugin"
+include("plugin")
