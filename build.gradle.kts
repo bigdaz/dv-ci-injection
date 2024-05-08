75ce88278b1a99e3099ec0a38a9f2cc5a8071af7
daz <daz@gradle.com>
2024-05-08 17:56:43 -0600

    Add workflow to promote script

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,10 +29,10 @@ testing {
     }
 }
 
-tasks.register<Copy>("publish") {
+tasks.register<Copy>("promote") {
     val version: String = project.property("version") as String
     inputs.property("version", version)
-    
+
     from("src/main/resources/develocity-auto-injection.init.gradle") {
         filter { line: String ->
             line.replace("<<version>>", version, false)
